e02aedf53c449839a12c4d5bdf7a315bde4f9524
1slander <hectorgomendez@gmail.com>
2023-03-05 21:31:38 +0000

    done - add a way to restartgame

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
     startGame();
-    setInterval(createObstacle, 60);
   };
 };
 
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
-const myObstacles = [];
+let myObstacles = [];
 
 let frames = 0;
 let score = 0;
@@ -18,6 +17,11 @@ let speedX = 5;
 
 let posX = 220;
 let posY = 450;
+
+let objectInterval;
+let gameInterval;
+
+let gameOver = false;
 
 const roadImg = new Image();
 roadImg.src = "images/road.png";
@@ -46,18 +50,49 @@ function startGame() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(carImg, 220, 450, 60, 120);
+  objectInterval = setInterval(() => {
+    createObstacle();
+    update();
+  }, 2000);
+  gameInterval = setInterval(update, 1000 / 60);
 }
 
-function clear() {
+function update() {
+  score++;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(carImg, posX, posY, 60, 120);
-}
+  ctx.font = "19px Arial";
+  ctx.fillText(`Score: ${score}`, 10, 50);
 
-function newPosition(x, y, width, height) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "#870007";
-  ctx.fillRect(x, y, width, height);
+  myObstacles.forEach((obstacle) => {
+    obstacle.draw();
+    obstacle.moveVertically();
+    if (
+      posX < obstacle.x + obstacle.width &&
+      posX + carImg.width > obstacle.x &&
+      posY < obstacle.y + obstacle.height &&
+      posY + carImg.height > obstacle.y
+    ) {
+      console.log("CRASH!");
+      clearInterval(objectInterval);
+      clearInterval(gameInterval);
+      ctx.fillStyle = "black";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = "white";
+      ctx.font = "48px Arial";
+      ctx.fillText("Game Over", 125, 300);
+      ctx.fillStyle = "#870007";
+      ctx.font = "38px Arial";
+      ctx.fillText(`Score: ${score}`, 150, 450);
+    }
+  });
+
+  myObstacles = myObstacles.filter((obstacle) => obstacle.y < canvas.height);
+}
+function createObstacle() {
+  let x = Math.floor(Math.random() * 350);
+  myObstacles.push(new Obstacle(x));
 }
 
 document.addEventListener("keydown", (e) => {
@@ -74,103 +109,5 @@ document.addEventListener("keydown", (e) => {
   } else if (posX > 598 - carImg.width) {
     posX = 598 - carImg.width;
   }
-  clear();
+  update();
 });
-
-// function createObstacle() {
-//   let x = Math.floor(Math.random() * canvas.width);
-//   let y = 0;
-//   let height = 20;
-//   let minWidth = 80;
-//   let maxWidth = 300;
-//   let width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
-//   newPosition(x, y, width, height);
-// }
-
-// for (i = 0; i < myObstacles.length; i++) {
-//   myObstacles[i].y += -1;
-//   myObstacles[i].update();
-// }
-//   frames += 1;
-//   if (frames % 120 === 0) {
-//     let x = Math.floor(Math.random() * canvas.width);
-//     let y = 0;
-//     let height = 20;
-//     let minWidth = 80;
-//     let maxWidth = 300;
-//     let width = Math.floor(
-//       Math.random() * (maxWidth - minWidth + 1) + minWidth
-//     );
-//     myObstacles.push(new Obstacle(x, y, width, height));
-//     for (i = 0; i < myObstacles.length; i++) {
-//       ctx.fillStyle = "#870007";
-//       ctx.fillRect(x, y, width, height);
-//       y += speedY;
-//     }
-//   }
-//   //}
-//   console.log(myObstacles);
-// }
-
-// window.onload = () => {
-//   document.getElementById("start-button").onclick = () => {
-//     startGame();
-//     updateObstacles();
-//     randomSize();
-//   };
-
-//   const myObstacles = [];
-//   const canvas = document.querySelector("#canvas");
-//   const ctx = canvas.getContext("2d");
-//   let posX = 220;
-//   let posY = 450;
-
-//   function startGame() {
-//     const roadImg = new Image();
-//     roadImg.src = "images/road.png";
-//     const carImg = new Image();
-//     carImg.src = "images/car.png";
-
-//     roadImg.onload = function () {
-//       ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
-//       ctx.drawImage(carImg, posX, posY, 60, 120);
-//     };
-
-//     document.addEventListener("keydown", (e) => {
-//       switch (e.key) {
-//         case "ArrowLeft":
-//           posX -= 10;
-
-//           break;
-//         case "ArrowRight":
-//           posX += 10;
-//           console.log(posX + carImg.width);
-//           break;
-//       }
-//       if (posX < 0) {
-//         posX = 0;
-//       } else if (posX > 598 - carImg.width) {
-//         posX = 598 - carImg.width;
-//       }
-//       ctx.clearRect(0, 0, canvas.width, canvas.height);
-//       ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
-//       ctx.drawImage(carImg, posX, posY, 60, 120);
-//     });
-//   }
-
-//   function updateObstacles() {
-//     randomSize();
-//     ctx.fillStyle = "red";
-//     ctx.fillRect(this.x, this.y, this.width, this.height);
-//   }
-
-//   function randomSize() {
-//     random = {
-//       x: Math.floor(Math.random() * (canvas.width + 1)),
-//       y: Math.floor(Math.random() * canvas.height + 1),
-//       width: Math.floor(Math.random() * (canvas.width - 158)),
-//       height: 20,
-//     };
-//     console.log(random);
-//   }
-// };
